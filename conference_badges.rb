a25d3d2a15fca8436fd1c6e64679e48f49a62d76
Justin Daniel <justinddaniel@gmail.com>
2017-09-18 20:28:18 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,9 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
-end 
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each {|name| badges << "Hello, my name is #{name}."}
+  badges
+end
